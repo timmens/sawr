5bf5626b0dda0a534c00fe948df93ef85507f3e2
Dominik Liebl <liebl.dominik@gmail.com>
2020-05-19 19:43:52 +0200

    updates saw_fun()
    only minor stuff: adding the package names of all functions

--- R/FUN_MAIN.R
+++ R/FUN_MAIN.R
@@ -5,37 +5,41 @@
 #' @param s.thresh A tuning parameter.
 #' @export
 saw_fun <- function(formula, dot = FALSE, s.thresh = NULL) {
-  results <- BKSGL.pdm.default(formula, s.thresh)
+
+  # formula = data$Y ~ data$X1 + data$X2
+  results <- sawr:::BKSGL.pdm.default(formula, s.thresh)
 
   x.all.matrix <- results$x.all.matrix
   y.matrix     <- results$y.matrix
   tausList     <- results$tausList
 
-  T <- nrow(y.matrix)
-  tausList <- lapply(tausList, function(tauVec) {
-                        if (length(tauVec)) {
+  T <- base::nrow(y.matrix)
+  tausList <- base::lapply(tausList, function(tauVec) {
+                        if (base::length(tauVec)) {
                           tauVec
                         } else {
                           NA
                         }
                     })
 
-  tausList <- lapply(tausList, function(tau_vect) tau_vect - 1)
-  tausList <- lapply(tausList, function(tauVec) tauVec[!tauVec %in% c(1, T)])
+  tausList <- base::lapply(tausList, function(tau_vect) tau_vect - 1)
+  tausList <- base::lapply(tausList, function(tauVec) tauVec[!tauVec %in% c(1, T)])
 
-  linear_model_data <- construct_data_for_linear_model(y.matrix, x.all.matrix,
-                                                       tausList, dot)
-  lm_fit_model      <- lm.fit(linear_model_data$X, linear_model_data$Y)
+  # tauVec<-tausList[[2]]; tauVec[!tauVec %in% c(1, T)]
+
+  linear_model_data <- sawr:::construct_data_for_linear_model(y.matrix, x.all.matrix,
+                                                              tausList, dot)
+  lm_fit_model      <- stats::lm.fit(linear_model_data$X, linear_model_data$Y)
   coeff             <- lm_fit_model$coefficients
 
-  posit   <- cumsum(sapply(tausList, function(tau_vect) sum(!is.na(tau_vect)) + 1))
+  posit   <- base::cumsum(base::sapply(tausList, function(tau_vect) base::sum(!base::is.na(tau_vect)) + 1))
   posit   <- c(0, posit)
   coeffList <- list()
-  for (i in seq_along(posit)[-1]) {
+  for (i in base::seq_along(posit)[-1]) {
     coeffList[[i-1]] <- coeff[(posit[i-1] + 1):posit[i]]
   }
 
-  betaMat  <- construct_beta(coeffList, tausList, nrow(x.all.matrix))
+  betaMat  <- sawr:::construct_beta(coeffList, tausList, base::nrow(x.all.matrix))
 
   list(betaMat = betaMat, tausList = tausList, coeffList = coeffList,
        X = linear_model_data$X)
